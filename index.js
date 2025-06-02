2ffd9f256d32a09f55c8f5b140636ca04c0fe814
Joeldaddy <23008962@myrp.edu.sg>
2025-06-02 13:04:44 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js - Entry point for demo_node project
+// Placeholder function for demonstration
+
+function placeholder() {
+    // TODO: Implement functionality
+    console.log('This is a placeholder function.');
+}
+
+// Call the placeholder function
+demo();
+
+// ...existing code...
